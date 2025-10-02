a5b164ac3b0931838c202869fb05cde87170dc16
LeoReiDelas2 <leonardodshow10@gmail.com>
2025-10-02 19:46:22 -0300

    Deixando os cadastros mais arrumados e jeitosos. UPDATE!!!

--- Trabalho OO/src/Services/Menus.java
+++ Trabalho OO/src/Services/Menus.java
@@ -6,26 +6,60 @@ import java.util.Scanner;
 
 public class Menus
 {
-    public void menuCadastros(Scanner scanner, Hospital Hospital)
-    {
-        System.out.print("Seja bem-vindo ao menu de cadastro!\n");
-        Integer escolha = InputHandler.digitarIntIntervalo("" +
-                "Digite 1 para cadastrar um paciente;\n" +
-                "Digite 2 para cadastrar um médico;\n" +
-                "Digite 3 para cadastrar um plano de saúde;\n" +
-                "Digite 0 para voltar ao menu anterior.", scanner, 0, 3);
+    public int exibirMenuPrincipal(Scanner scanner, Hospital hospital) {
+        System.out.println("\n=== Sistema de Gerenciamento Hospitalar ===");
+        System.out.println("1 - Menu de Cadastros");
+        System.out.println("2 - Menu de Operações (Agendamentos, etc.)");
+        System.out.println("3 - Menu de Relatórios");
+        System.out.println("0 - Sair do Sistema");
+        System.out.println("========================================");
+
+        int escolha = InputHandler.digitarIntIntervalo("Digite a sua opção: ", scanner, 0, 3);
+
         switch (escolha) {
-            case 0:
-                return;
             case 1:
-                Hospital.cadastrarPaciente(scanner);
+                menuCadastros(scanner, hospital);
                 break;
             case 2:
-                Hospital.cadastrarMedico(scanner);
+                System.out.println("--> nada");
                 break;
             case 3:
-                Hospital.cadastrarPlanoDeSaude(scanner);
+                System.out.println("--> nada");
                 break;
-            }
+            case 0:
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+        return escolha;
     }
+    public static void menuCadastros(Scanner scanner, Hospital hospital) {
+        while (true) {
+            System.out.println("\n--- Menu de Cadastros ---");
+            System.out.println("1. Cadastrar Paciente");
+            System.out.println("2. Cadastrar Médico");
+            System.out.println("3. Cadastrar Plano de Saúde");
+            System.out.println("0. Voltar ao Menu Principal");
+            System.out.println("-------------------------");
+            int escolha = InputHandler.digitarIntIntervalo("Digite a sua opção: ", scanner, 0, 3);
+            switch (escolha) {
+                case 0:
+                    return;
+                case 1:
+                    hospital.cadastrarPaciente(scanner);
+                    break;
+                case 2:
+                    hospital.cadastrarMedico(scanner);
+                    break;
+                case 3:
+                    hospital.cadastrarPlanoDeSaude(scanner);
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        }
+    }
+
 }
--- Trabalho OO/src/application/Main.java
+++ Trabalho OO/src/application/Main.java
@@ -11,33 +11,9 @@ public class Main {
         Menus Menus = new Menus();
         Hospital hospital = new Hospital();
         Scanner sc = new Scanner(System.in);
-
         int escolha;
-        //Aqui é o menu principal
         do{
-            System.out.println("BEM VINDO AO MENU\ndigite 1 para o modo cadastro\ndigite 2 para modo medico");
-            escolha = InputHandler.digitarIntIntervalo("digite um inteiro entre 0 e 2", sc,0,2);
-
-            switch (escolha)
-            {
-                case 1:
-                    Menus.menuCadastros(sc, hospital);
-                    break;
-
-                case 2:
-                    break;
-
-                case 0:
-                    System.out.println("digitou 0");
-                    sc.close();
-                    System.exit(0);
-                    break;
-
-                default:
-                    System.out.println("digitou outro num");
-                    break;
-
-            }
+            escolha = Menus.exibirMenuPrincipal(sc, hospital);
         }while(escolha !=0);
     }
 }
